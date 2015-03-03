85ab435caf086059949a016d7263093efa9b7d9d
Ryan Lahfa <masterancpp@gmail.com>
2015-03-03 11:55:11 +0100

    Clean up old code (which usse unused dep and incorrect paths)

--- src/server.js
+++ src/server.js
@@ -1,17 +1,12 @@
 var http = require('http');
 var fs = require('fs');
-var index = fs.readFileSync('index.html');
-var app = fs.readFileSync('app.js');
-var libs = fs.readFileSync('libs.js');
+var index = fs.readFileSync('src/index.html');
+var app = fs.readFileSync('src/app.js');
 
 var server = http.createServer(function (req, res) {
     if (req.url === '/app.js') {
         res.writeHead(200, {'Content-Type': 'text/javascript'});
         res.end(app);
-    }
-    else if (req.url === '/libs.js') {
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.end(libs);
     }
     else {
         res.writeHead(200, {'Content-Type': 'text/html'});
@@ -20,7 +15,6 @@ var server = http.createServer(function (req, res) {
 });
 
 var io = require('socket.io').listen(server);
-var ss = require('socket.io-stream');
 var path = require('path');
 var tesseract = require('node-tesseract');
 
